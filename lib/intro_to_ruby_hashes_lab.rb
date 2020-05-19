f9364c1ae3de6f0d3e1725d76d388e0783d280d0
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 18:26:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,9 +9,9 @@ def read_from_hash(hash, key)
 
 
 def update_counting_hash(hash, key)
- if hash[:key]
- hash[:key]+=1
- else hash[:key]=1
+ if hash[key]
+ hash[key]+=1
+ else hash[key]=1
  end
  hash
 end
